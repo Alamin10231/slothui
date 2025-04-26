4d4c2c909554eb0df2d03cdf2423ffd8f0b4c45d
Md Alamin <alamin3636md@example.com>
2025-04-26 16:30:41 +0600

    almost done now checking process

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-
-  document.querySelectorAll('.question-content').forEach(item => {
-    item.addEventListener('click', () => {
-      item.classList.toggle('active');
-    });
+document.querySelectorAll(".question-content").forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active");
   });
+});
 
+const scrollButton = document.getElementById("scrollToTop");
+
+scrollButton.addEventListener("click", function () {
+  console.log("kanamiya");
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
